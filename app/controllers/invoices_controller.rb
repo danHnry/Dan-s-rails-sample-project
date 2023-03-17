a4c28e1960062aa31315a1deb3fc98633884c30e
Danial Raeen Najad <danialnajad@Hnry.io>
2023-03-17 11:56:24 +1100

    Finished adding a new article and displaying it on the main page

---
+++ app/controllers/invoices_controller.rb
@@ -0,0 +1,50 @@
+class InvoicesController < ApplicationController
+  def index
+    @invoices = Invoice.all
+  end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
+
+  def new
+    @invoice = Invoice.new
+  end
+
+  def create
+    @invoice = Invoice.new(invoice_params)
+
+    if @invoice.save
+      redirect_to @invoice
+    else
+      render :new, status: :unprocessable_entity
+    end
+ end
+
+ def edit
+  @invoice = Invoice.find(params[:id])
+ end
+
+ def update
+  @invoice = Invoice.find(params[:id])
+
+  if @invoice.update(invoice_params)
+    redirect_to @invoice
+  else
+    render :edit, status: :unprocessable_entity
+  end
+ end
+
+ def destroy
+  @invoice = Invoice.find(params[:id])
+  @invoice.destroy
+
+  redirect_to root_path, status: :see_other
+end
+
+
+   private
+    def invoice_params
+      params.require(:invoice).permit(:name, :description)
+    end
+end
